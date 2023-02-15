f706c1c36321261a967847c0450f6cff4c3db418
t.mazitov <t.mazitov@intec-balance.ru>
2023-02-15 09:49:12 +0500

    16 Webpack hot module replacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,10 +3,11 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html
         }),
@@ -19,4 +20,11 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
             __IS_DEV__: JSON.stringify(isDev)
         }),
     ]
+
+    if (isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin());
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+    }
+
+    return plugins;
 }
