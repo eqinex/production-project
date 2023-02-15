6082d29fec4d9add264b5c2a42b7279bb9f2fdf5
t.mazitov <t.mazitov@intec-balance.ru>
2023-02-15 10:42:05 +0500

    17 Babel. Extract plugin [optional]

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -43,7 +43,19 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         ],
     }
 
+    const babelLoader = {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    }
+
     return [
+        babelLoader,
         typescriptLoader,
         cssLoader,
         svgLoader,
