f67a3813496e692936d97992716de4f76f9888ed
t.mazitov <t.mazitov@intec-balance.ru>
2023-02-28 19:18:55 +0500

    27 CI pipeline. Автоматизация прогона тестов

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -2,5 +2,6 @@ import { lazy } from 'react';
 
 export const AboutPageAsync = lazy(() => new Promise((resolve) => {
     // @ts-ignore
+    // В реальных проектах так не делать!
     setTimeout(() => resolve(import('./AboutPage')), 1500);
 }));
--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -2,5 +2,6 @@ import { lazy } from 'react';
 
 export const MainPageAsync = lazy(() => new Promise((resolve) => {
     // @ts-ignore
+    // В реальных проектах так не делать!
     setTimeout(() => resolve(import('./MainPage')), 1500);
 }));
