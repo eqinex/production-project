c4c13da12c4ed59cae9945bf1a8bb7ef598ac659
t.mazitov <t.mazitov@intec-balance.ru>
2023-03-03 14:17:06 +0500

    29 Модальное окно. Portal

---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import Modal from './Modal';
+
+export default {
+    title: 'shared/Modal',
+    component: Modal,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda beatae consequatur deleniti, dolorem molestiae soluta suscipit totam ullam voluptate!',
+    isOpen: true,
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda beatae consequatur deleniti, dolorem molestiae soluta suscipit totam ullam voluptate!',
+    isOpen: true,
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,38 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import React, { useCallback, useState } from 'react';
+import Modal from 'shared/ui/Modal/Modal';
+import { useTranslation } from 'react-i18next';
+import Button, { ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            /
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/* eslint-disable-next-line max-len */}
+                {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda beatae consequatur deleniti, dolorem molestiae soluta suscipit totam ullam voluptate!')}
+            </Modal>
+
         </div>
-    </div>
-);
+    );
+};
 
 export default Navbar;
