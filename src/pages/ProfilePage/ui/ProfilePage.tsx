38c7fd0b2d2a9f8f66a47e948930898cbd83b801
t.mazitov <t.mazitov@intec-balance.ru>
2023-03-22 11:26:50 +0500

    39 Модуль профиля. Фетчинг данных. TS strict mode

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,7 +1,13 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { profileReducer } from 'entities/Profile';
+import {
+    fetchProfileData,
+    ProfileCard,
+    profileReducer,
+} from 'entities/Profile';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useEffect } from 'react';
 
 interface ProfilePageProps {
     className?: string;
@@ -13,11 +19,16 @@ const initialReducers: ReducersList = {
 
 const ProfilePage = ({ className }: ProfilePageProps) => {
     const { t } = useTranslation('profile');
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
 
     return (
         <DynamicModuleLoader reducers={initialReducers} removeAfterUnmount>
             <div className={classNames('', {}, [className])}>
-                {t('Страница профиля')}
+                <ProfileCard />
             </div>
         </DynamicModuleLoader>
     );
