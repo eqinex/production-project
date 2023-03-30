61e0bd4b8e8a1f34630134511c8a761944d6d382
t.mazitov <t.mazitov@intec-balance.ru>
2023-03-30 12:59:38 +0500

    42 Валидация профиля. Коды ошибок

--- src/entities/Profile/model/service/updateProfileData/updateProfileData.ts
+++ src/entities/Profile/model/service/updateProfileData/updateProfileData.ts
@@ -1,9 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider';
-import { Profile } from '../../types/profile';
+import { Profile, ValidateProfileError } from '../../types/profile';
 import { getProfileForm } from '../../selectors/getProfileForm/getProfileForm';
+import { validateProfileData } from '../../service/validateProfileData/validateProfileData';
 
-export const updateProfileData = createAsyncThunk<Profile, void, ThunkConfig<string>>(
+export const updateProfileData = createAsyncThunk<
+    Profile,
+    void,
+    ThunkConfig<ValidateProfileError[]>
+>(
     'profile/updateProfileData',
     async (_, thunkApi) => {
         const {
@@ -13,14 +18,18 @@ export const updateProfileData = createAsyncThunk<Profile, void, ThunkConfig<str
         } = thunkApi;
 
         const formData = getProfileForm(getState());
+        const errors = validateProfileData(formData);
 
+        if (errors.length) {
+            return rejectWithValue(errors);
+        }
         try {
             const response = await extra.api.put<Profile>('/profile', formData);
 
             return response.data;
         } catch (e) {
             console.log(e);
-            return rejectWithValue('error');
+            return rejectWithValue([ValidateProfileError.SERVER_ERROR]);
         }
     },
 );
