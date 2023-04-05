835f364793d17999a0fd15a3bf2f0b92e3b3b13d
t.mazitov <t.mazitov@intec-balance.ru>
2023-04-05 11:31:59 +0500

    48 ArticlesPage и ArticleDetailsPage. Декомпозиция. Сущности. Webpack publicPath

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
@@ -0,0 +1,7 @@
+import { lazy } from 'react';
+
+export const ArticleDetailsPageAsync = lazy(() => new Promise((resolve) => {
+    // @ts-ignore
+    // В реальных проектах так не делать!
+    setTimeout(() => resolve(import('./ArticleDetailsPage')), 1500);
+}));
---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
@@ -0,0 +1,7 @@
+import { lazy } from 'react';
+
+export const ArticlesPageAsync = lazy(() => new Promise((resolve) => {
+    // @ts-ignore
+    // В реальных проектах так не делать!
+    setTimeout(() => resolve(import('./ArticlesPage')), 1500);
+}));
