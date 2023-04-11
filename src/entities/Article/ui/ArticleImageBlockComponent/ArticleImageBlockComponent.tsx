b30a8ca9c8754f6d3a51c281e0086e9f9ba2d2d1
t.mazitov <t.mazitov@intec-balance.ru>
2023-04-11 11:30:27 +0500

    50.1 Страница статьи. Блочная структура. Компонент Code. Копирование

--- src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -1,6 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { Text, TextAlign } from 'shared/ui/Text/Text';
 import cls from './ArticleImageBlockComponent.module.scss';
 import { ArticleImageBlock } from '../../model/types/article';
 
@@ -18,7 +19,10 @@ export const ArticleImageBlockComponent = memo((props: ArticleImageBlockComponen
 
     return (
         <div className={classNames(cls.ArticleImageBlockComponent, {}, [className])}>
-            ArticleImageBlockComponent
+            <img src={block.src} alt={block.title} className={cls.img} />
+            {block.title && (
+                <Text text={block.title} align={TextAlign.CENTER} />
+            )}
         </div>
     );
 });
