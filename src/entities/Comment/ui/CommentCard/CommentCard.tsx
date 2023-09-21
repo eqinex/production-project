2ba34afd609b61c4e227b3cb314a1b4e47faa92e
t.mazitov <t.mazitov@intec-balance.ru>
2023-09-21 17:21:50 +0500

    51 Модуль комментариев. Нормализация данных. EntityAdapter

---
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -0,0 +1,30 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Text } from 'shared/ui/Text/Text';
+import { Comment } from '../../model/types/comment';
+import cls from './CommentCard.module.scss';
+
+interface CommentCardProps {
+    className?: string;
+    comment: Comment;
+    isLoading?: boolean;
+}
+
+export const CommentCard = memo((props: CommentCardProps) => {
+    const {
+        className,
+        comment,
+        isLoading,
+    } = props;
+
+    return (
+        <div className={classNames(cls.CommentCard, {}, [className])}>
+            <div className={cls.header}>
+                {comment.user.avatar ? <Avatar size={30} src={comment.user.avatar} /> : null }
+                <Text className={cls.username} title={comment.user.username} />
+            </div>
+            <Text className={cls.text} text={comment.text} />
+        </div>
+    );
+});
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -3,6 +3,9 @@ import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { ArticleDetails } from 'entities/Article';
 import { useParams } from 'react-router-dom';
+import { Text } from 'shared/ui/Text/Text';
+import { CommentList } from 'entities/Comment';
+import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -24,8 +27,30 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     }
 
     return (
-        <div className={classNames('', {}, [className])}>
+        <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
             <ArticleDetails id={id} />
+            <Text className={cls.commentTitle} title={t('Комментарий')} />
+            <CommentList comments={[
+                {
+                    id: 1,
+                    text: 'comment 1',
+                    user: {
+                        id: 1,
+                        username: 'eqinex',
+                        avatar: 'https://i.ebayimg.com/images/g/smkAAOSw0AZj4NDR/s-l400.jpg',
+                    },
+                },
+                {
+                    id: 2,
+                    text: 'comment 2',
+                    user: {
+                        id: 1,
+                        username: 'eqinex',
+                        avatar: 'https://i.ebayimg.com/images/g/smkAAOSw0AZj4NDR/s-l400.jpg',
+                    },
+                },
+            ]}
+            />
         </div>
     );
 };
